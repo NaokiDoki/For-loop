b98f9bc78055663b59dd990c230d17178bc65867
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-06-08 07:00:16 +0545

    Create printing first 20 whole numb and first 20 natural numb.c

---
+++ printing first 20 whole numb and first 20 natural numb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+  int a;
+  printf("First 20 whole numbers are:\n");
+  for(a = 0; a <= 19; a++){
+    printf("%d\t", a);
+    }
+  printf("\nFirst 20 natural numbers are:\n");
+  for(a = 1; a <= 20; a++){
+  	printf("%d\t", a);
+  }	
+  return 0;
+}
